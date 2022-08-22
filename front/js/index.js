b080b6a671bfc10e5094e0de681d7551583bee56
fire-walker <charlotte.churcheward@gmail.com>
2022-08-22 15:27:08 +0200

    Modif index.js

--- front/js/index.js
+++ front/js/index.js
@@ -10,25 +10,40 @@ fetch("http://localhost:3000/api/products")
         createProducts(products);
     })
     .catch((error) => {
-        alert(); // Une erreur est survenue
+        alert(error); // Une erreur est survenue
     });
 
 
-// Affichage des produits dans le DOM
-function createProducts(products) {
-    const sectionItems = document.getElementById('items');
-    let content = '';
+/*
+* Fonction d'affichage des produits sur la page
+*/
+function displayProducts(products) {
+    // On récupére l'élément items situé dans le DOM
+    const itemsContainer = document.getElementById('items');
+    const fragment = document.createDocumentFragment();
 
     for (let product of products) {
-        content += `
-                <a href="./product.html?id=${product._id}">
-                    <article>
-                        <img src="${product.imageUrl}" alt="${product.altTxt}">
-                        <h3 class="productName">${product.name}</h3>
-                        <p class="productDescription">${product.description}</p>
-                    </article>
-                </a>`;
+        const element = createProduct(product);
+        fragment.appendChild(element);
     }
 
-    sectionItems.innerHTML = content;
+    itemsContainer.appendChild(fragment);
+}
+
+/*
+* Fonction de création des produits de manière dynamique
+*/
+function createProduct(product) {
+    const template = document.createElement('template');
+    template.innerHTML = `
+        <a href="./product.html?id=${product._id}">
+            <article>
+                <img src="${product.imageUrl}" alt="${product.altTxt}">
+                <h3 class="productName">${product.name}</h3>
+                <p class="productDescription">${product.description}</p>
+            </article>
+        </a>
+    `;
+
+    return template.content.firstElementChild;
 }
