7031e1d86793cd28c28801f4660dbe95ac9ce5a1
fire-walker <charlotte.churcheward@gmail.com>
2022-09-05 15:14:50 +0200

    Refactorisation + modif affichage des produits stockés dans le LS + initialisation de l'affichage du prix récupéré via l'API

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,66 +1,107 @@
-const productsInCart = localStorage.length;
-const cart = [];
+const PRODUCTS_KEY_LOCALSTORAGE = 'products';
 
-displayCart();
-cart.forEach((product) => displayProducts(product));
+const cartTitle = document.getElementById('cart-title');
+let products = [];
 
-function displayCart() {
-
-    for (let i = 0; i < productsInCart; i++) {
-        cart.push(JSON.parse(localStorage.getItem("cart")));
-    }
+async function getProduct(id) {
+    console.log("debut fonction getproduct");
+    return await fetch(`http://localhost:3000/api/products/${id}`)
+        .then((res) => {
+            return res.json()
+        })
+        .then((product) => {
+            console.log("fin fonction getProduct");
+            return product;
+        })
+        .catch((error) => {
+            console.error("Fetch Error", error);
+        });
 }
 
-function displayProducts(products) {
+if (localStorageHas(PRODUCTS_KEY_LOCALSTORAGE)) {
+    products = JSON.parse(localStorage.getItem(PRODUCTS_KEY_LOCALSTORAGE));
 
+    displayProducts();
+} else {
+    cartTitle.textContent = 'Le panier est vide';
+}
+
+function displayProducts() {
     const itemsContainer = document.getElementById('cart__items');
     const fragment = document.createDocumentFragment();
 
-    for (let product of products) {
-        const element = createProduct(product);
+     // for (let product of products) {
+     //
+     //     const element = createProduct(product);
+     //     fragment.appendChild(element);
+     // }
+
+    function newProduct(product, response = "default value") {
+        console.log("début fonction testProduct");
+        // console.log(response);
+
+        const productFinal = product;
+        productFinal.price = response;
+        // console.log(productFinal);
+        //console.log(productTest);
+
+        const element = createProduct(productFinal);
         fragment.appendChild(element);
+        console.log("fin fonction testproduct");
     }
 
+    //console.log(products);
+
+    products.map(product => {
+        console.log(product);
+        console.log("item-mapping");
+
+        let productPrice = getProduct(product.id).price;
+        newProduct(product, productPrice);
+        console.log(productPrice);
+    });
+
+    console.log(fragment);
     itemsContainer.appendChild(fragment);
+    //console.log(itemsContainer);
 }
 
 function createProduct(product) {
     const template = document.createElement('template');
-
-    if (cart.length !== 0) {
-        template.innerHTML = `
-        <article class="cart__item" data-id="${product._id}" data-color="${product.color}">
-                <div class="cart__item__img">
-                  <img src="${product.imageUrl}" alt="${product.altTxt}">
+    template.innerHTML = `
+    <article class="cart__item" data-id="${product._id}" data-color="${product.color}">
+            <div class="cart__item__img">
+              <img src="${product.imageUrl}" alt="${product.altTxt}">
+            </div>
+            <div class="cart__item__content">
+              <div class="cart__item__content__description">
+                <h2>${product.title}</h2>
+                <p>${product.colors}</p>
+                <p>${product.price}</p>
+              </div>
+              <div class="cart__item__content__settings">
+                <div class="cart__item__content__settings__quantity">
+                  <p>Qté :</p>
+                  <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${product.quantity}">
                 </div>
-                <div class="cart__item__content">
-                  <div class="cart__item__content__description">
-                    <h2>${product.title}</h2>
-                    <p>${product.colors}</p>
-                    <p>${product.price}</p>
-                  </div>
-                  <div class="cart__item__content__settings">
-                    <div class="cart__item__content__settings__quantity">
-                      <p>Qté :</p>
-                      <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${product.quantity}">
-                    </div>
-                    <div class="cart__item__content__settings__delete">
-                      <p class="deleteItem">Supprimer</p>
-                    </div>
-                  </div>
+                <div class="cart__item__content__settings__delete">
+                  <p class="deleteItem">Supprimer</p>
                 </div>
-              </article>
+              </div>
+            </div>
+          </article>
     `;
-
-    }else{
-        template.innerHTML = `
-        <h2>Le panier est vide</h2>
-        <a href="index.html">
-            <button class="homebtn">Retour à l'accueil</button>
-        </a>`;
-    }
 
     return template.content.firstElementChild;
 }
 
 
+/**
+ * On vérifie que la clé existe dans le localStorage
+ * @param {String} key
+ * @return {Boolean}
+ */
+function localStorageHas(key) {
+    const item = localStorage.getItem(key);
+    return item !== null;
+}
