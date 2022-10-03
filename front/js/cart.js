fd94dd63d896969b519626f8dd80e84390e281a4
fire-walker <charlotte.churcheward@gmail.com>
2022-10-03 10:10:41 +0200

    Ajout de JSDoc + refactorisation du code, ajout d'une fonction init, modification des regex et de la fonction d'envoi du formulaire

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,25 +1,148 @@
 const PRODUCTS_KEY_LOCALSTORAGE = 'products';
 
 const cartTitle = document.getElementById('cart-title');
+const itemsContainer = document.getElementById('cart__items');
+const form = document.getElementById('formOrder');
+
 let products = JSON.parse(localStorage.getItem(PRODUCTS_KEY_LOCALSTORAGE));
 
-/** Affichage des produits stockés dans le LS **/
-
+/**
+ * Affichage des produits stockés dans le LS
+ */
 if (localStorageHas(PRODUCTS_KEY_LOCALSTORAGE)) {
-    displayProducts();
+    init();
 } else {
     cartTitle.textContent = 'Le panier est vide';
 }
 
-const itemsContainer = document.getElementById('cart__items');
+/**
+ * Fonction d'initialisation
+ * - Afficher les produits présents dans le Local Storage
+ * - Récupérer toutes les informations nécessaires afin de créer un objet contact
+ */
+function init() {
+    displayProducts();
 
+    const errorMessage = (tag, message, isValid) => {
+        const inputsError = document.querySelector('#' + tag + 'ErrorMsg');
+        if (isValid) {
+            inputsError.textContent = "\u2713";
+        } else {
+            inputsError.textContent = message;
+        }
+    };
+
+    let contact = {
+        firstName: null,
+        lastName: null,
+        address: null,
+        city: null,
+        email: null
+    };
+
+    /**
+     * Validation des inputs en utilisant des regex
+     */
+    const firstNameValidation = (value) => {
+        if (value.match(/^[A-Za-zÀ-ÿ '.-]{2,15}$/)) {
+            errorMessage('firstName', '', true);
+            contact.firstName = value;
+        } else {
+            errorMessage('firstName',
+                'La saisie du prénom est invalide. ' +
+                'Il doit contenir entre 2 et 15 caractères, les caractères spéciaux autres que les lettres accentuées, ".", "-" et "\'" ne sont pas autorisés.');
+            return false;
+        }
+    };
+    const lastNameValidation = (value) => {
+        if (value.match(/^[A-ZÀ-Ÿ '.-]{2,20}$/)) {
+            errorMessage('lastName', '', true);
+            contact.lastName = value;
+        } else {
+            errorMessage('lastName',
+                'La saisie du prénom est invalide. ' +
+                'Il doit contenir entre 2 et 15 caractères en majuscule, les caractères spéciaux autres que les lettres accentuées, ".", "-" et "\'" ne sont pas autorisés.');
+            return false;
+        }
+    };
+    const addressValidation = (value) => {
+        if (value.match(/^[A-Za-zÀ-ÿ0-9 '.,-]{8,45}$/)) {
+            errorMessage('address', '', true);
+            contact.address = value;
+        } else {
+            errorMessage ('address',
+                'La saisie de l\'adresse est invalide. ' +
+                'Elle doit contenir un numéro de rue ainsi que son nom, un lieu dit ou une commune.');
+            return false;
+        }
+    };
+    const cityValidation = (value) => {
+        if (value.match('Y') || value.match(/^[A-Za-zÀ-ÿ '.-]{2,45}$/)) {
+            errorMessage('city', '', true);
+            contact.city = value;
+        } else {
+            errorMessage('city',
+                'La saisie de la ville est invalide. ' +
+                'Elle doit contenir entre 2 et 45 caractères, les caractères spéciaux autorisés sont ".", "-" et "\'".');
+            return false;
+        }
+    };
+    const emailValidation = (value) => {
+        if (value.match(/^[a-z0-9 ._-]+@[a-z]+\.[a-z]{2,3}/)){
+            errorMessage('email', '', true);
+            contact.email = value;
+        } else {
+            errorMessage ('email',
+                'La saisie de l\'email est invalide. ' +
+                'L\'email doit être au format johndoe@gmail.com. Il peut contenir les caractères spéciaux "_", "-" et "." uniquement');
+            return false;
+        }
+    };
+
+    /**
+     * Ajout d'évènement sur chaque champs du formulaire
+     * Ajout d'un événement sur le bouton qui soumet le formulaire
+     */
+    form.firstName.addEventListener('input', (e) => {
+        firstNameValidation(e.target.value);
+    });
+    form.lastName.addEventListener('input', (e) => {
+        lastNameValidation(e.target.value);
+    });
+    form.address.addEventListener('input', (e) => {
+        addressValidation(e.target.value);
+    });
+    form.city.addEventListener('input', (e) => {
+        cityValidation(e.target.value);
+    });
+    form.email.addEventListener('input', (e) => {
+        emailValidation(e.target.value);
+    });
+
+    const submitButton = document.getElementById('order');
+    submitButton.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        if (contact.firstName && contact.lastName && contact.address && contact.city && contact.email) {
+            sendOrder(contact);
+        }else{
+            alert("Merci de remplir tous les champs du formulaire.");
+        }
+    });
+}
+
+/**
+ * Fonction d'affichage des produits sur la page
+ */
 function displayProducts() {
     const fragment = document.createDocumentFragment();
 
     for (let i = 0; i < products.length; i++) {
         const product = products[i];
 
-        /** Fetch API pour récupérer le prix des produits qui n'est pas stocké dans le LS **/
+        /**
+         * Fetch API pour récupérer le prix des produits qui n'est pas stocké dans le LS
+         */
         fetch(`http://localhost:3000/api/products/` + product.id)
             .then((res) => {
                 return res.json()
@@ -60,7 +183,9 @@ function displayProducts() {
     }
 }
 
-/** Création des produits du LS **/
+/**
+ * Fonction de création des produits stockés dans le Local Storage de manière dynamique
+ */
 function createProduct(product) {
     const template = document.createElement('template');
     template.innerHTML = `
@@ -100,7 +225,12 @@ function localStorageHas(key) {
     return item !== null;
 }
 
-/** Suppression de produit **/
+/**
+ * Fonction de suppression des produits
+ * - Récupérer l'id et la couleur du produit voulu
+ * - Modification du contenu du tableau en supprimant le produit
+ * - Affichage des nouveaux totaux prix et quantité
+ */
 
 function removeProduct(event) {
     const article = event.target.closest("article");
@@ -118,7 +248,11 @@ function removeProduct(event) {
     displayCartQuantity();
 }
 
-/** Update de quantité **/
+/**
+ * Fonction de suppression des produits
+ * - Récupérer l'id et la couleur du produit voulu
+ * - Affichage des nouveaux totaux prix et quantité
+ */
 
 function changeQuantity(event) {
     const article = event.target.closest("article");
@@ -142,14 +276,14 @@ function changeQuantity(event) {
     displayCartQuantity();
 }
 
-/** Affichage des totaux prix et quantité **/
+/**
+ * Fonction d'affichage du total prix
+ */
 function displayCartPrice() {
     let totalPrice = 0;
     const cartPrice = document.getElementById('totalPrice');
 
     products.forEach((product) => {
-        console.log(product.price)
-        console.log(product.quantity)
         const totalProductPrice = product.price * product.quantity;
         totalPrice += totalProductPrice;
     });
@@ -157,6 +291,9 @@ function displayCartPrice() {
     cartPrice.innerHTML = totalPrice;
 }
 
+/**
+ * Fonction d'affichage du total quantité
+ */
 function displayCartQuantity() {
     let totalQuantity = 0;
     const cartQuantity = document.getElementById('totalQuantity');
@@ -169,144 +306,40 @@ function displayCartQuantity() {
     cartQuantity.innerHTML = totalQuantity;
 }
 
-/** User form **/
-
-let inputFirstName = document.getElementById('firstName');
-let inputLastName = document.getElementById('lastName');
-let inputAddress = document.getElementById('address');
-let inputCity = document.getElementById('city');
-let inputEmail = document.getElementById('email');
-
-function orderForm() {
-
-    const errorMessage = (tag, message, isValid) => {
-        const inputsError = document.querySelector('#' + tag + 'ErrorMsg');
-        if (isValid) {
-            inputsError.textContent = "\u2713";
-        } else {
-            inputsError.textContent = message;
-        }
+/**
+ * Fonction d'envoi de la commande
+ * - Stocker des informations nécessaire dans l'objet contact
+ * - Envoiyer les données à l'API
+ * - Récuperer l'orderId fournit grâce à la méthode POST
+ * - Rediriger vers la page de confirmation avec l'orderId présent dans l'url
+ */
+function sendOrder(contact) {
+    const body = {
+        contact: {
+            firstName: contact.firstName,
+            lastName: contact.lastName,
+            city: contact.city,
+            address: contact.address,
+            email: contact.email,
+        },
+        products: products.map(product => product.id),
     };
 
-    /** Validation des inputs en utilisant des regex **/
-
-    const firstNameValidation = (value) => {
-        if (value.match(/^[A-Za-zÀ-ÿ '.-]{2,15}$/)) {
-            errorMessage('firstName', '', true);
-            inputFirstName = value;
-        } else {
-            errorMessage('firstName',
-                'La saisie du prénom est invalide. ' +
-                'Il doit contenir entre 2 et 15 caractères, les caractères spéciaux autres que les lettres accentuées, ".", "-" et "\'" ne sont pas autorisés.');
-            return false;
-        }
-    };
-
-    const lastNameValidation = (value) => {
-        if (value.match(/^[A-ZÀ-Ÿ '.-]{2,20}$/)) {
-            errorMessage('lastName', '', true);
-            inputLastName = value;
-        } else {
-            errorMessage('lastName',
-                'La saisie du prénom est invalide. ' +
-                'Il doit contenir entre 2 et 15 caractères en majuscule, les caractères spéciaux autres que les lettres accentuées, ".", "-" et "\'" ne sont pas autorisés.');
-            return false;
-        }
-    };
-
-    const addressValidation = (value) => {
-        if (value.match(/^([0-9]*) ?([A-Za-zÀ-ÿ0-9 '.,-]*) ?$/)) {
-            errorMessage('address', '', true);
-            inputAddress = value;
-        } else {
-            errorMessage ('address',
-                'La saisie de l\'adresse est invalide. ' +
-                'Elle doit contenir un numéro de rue ainsi que son nom.');
-            return false;
-        }
-    };
-
-    const cityValidation = (value) => {
-        if (value.match('Y') || value.match(/^[A-Za-zÀ-ÿ '.-]{2,45}$/)) {
-            errorMessage('city', '', true);
-            inputCity = value;
-        } else {
-            errorMessage('city',
-                'La saisie de la ville est invalide. ' +
-                'Il doit contenir entre 2 et 45 caractères, les caractères spéciaux autorisés sont ".", "-" et "\'".');
-            return false;
-        }
-    };
-
-    const emailValidation = (value) => {
-        if (value.match(/^[a-z0-9 ._-]+@[a-z]+\.[a-z]{2,3}/)){
-            errorMessage('email', '', true);
-            inputEmail = value;
-        } else {
-            errorMessage ('email',
-                'La saisie de l\'email est invalide. ' +
-                'L\'email doit être au format johndoe@gmail.com. Il peut contenir les caractères spéciaux "_", "-" et "." uniquement');
-            return false;
-        }
-    };
-
-    inputFirstName.addEventListener('input', (e) => {
-        firstNameValidation(e.target.value);
-    });
-    inputLastName.addEventListener('input', (e) => {
-        lastNameValidation(e.target.value);
-    });
-    inputAddress.addEventListener('input', (e) => {
-        addressValidation(e.target.value);
-    });
-    inputCity.addEventListener('input', (e) => {
-        cityValidation(e.target.value);
-    });
-    inputEmail.addEventListener('input', (e) => {
-        emailValidation(e.target.value);
-    });
-}
-
-function postOrderForm() {
-    const submitButton = document.getElementById('order');
-    submitButton.addEventListener('click', (e) => {
-        e.preventDefault();
-
-        if (inputFirstName && inputLastName && inputAddress && inputCity && inputEmail) {
-            // let orderProducts = [];
-            // products: products.map(product => product.id);
-
-            const order = {
-                contact: {
-                    firstName: inputFirstName.value,
-                    lastName: inputLastName.value,
-                    city: inputCity.value,
-                    address: inputAddress.value,
-                    email: inputEmail.value,
-                },
-                products: products.map(product => product.id),
-            };
-            console.log(products);
-
-            const options = {
-                method: "POST",
-                body: JSON.stringify(order),
-                headers: { "Content-Type": "application/json"},
-            };
-
-            fetch("http://localhost:3000/api/products/order", options)
-                .then((res) => res.json())
-                .then((data) => {
-                    const orderId = data.orderId;
-                    window.location.href = "confirmation.html" + "?orderId=" + orderId;
-
-                })
-                .catch(function (error) {
-                    console.error("Fetch Error", error);
-                });
+    fetch("http://localhost:3000/api/products/order", {
+        method: "POST",
+        body: JSON.stringify(body),
+        headers: {
+            "Content-Type": "application/json"
         }
     })
-}
+        .then((res) => res.json())
+        .then((data) => {
+            const orderId = data.orderId;
 
-orderForm();
-postOrderForm();
+            localStorage.removeItem(PRODUCTS_KEY_LOCALSTORAGE);
+            window.location.href = "confirmation.html" + "?orderId=" + orderId;
+        })
+        .catch(function (error) {
+            console.error("Fetch Error", error);
+        });
+}
